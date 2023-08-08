ecc14ce800691181d0501255176c288697573251
itani <itani@ANNIE-PC>
2023-08-07 20:31:08 -0400

    changes

--- Project 3/anagrams.cpp
+++ Project 3/anagrams.cpp
@@ -1,168 +1,227 @@
+#include <functional>
 #include <iostream>
-#include <fstream>
-#include <istream>
-#include <cstring>
+#include <string>
 #include <vector>
 
 using namespace std;
 
-const int MAXRESULTS = 20;    // Max matches that can be found
-const int MAXDICTWORDS = 30000; // Max words that can be read in
+const int MAXRESULTS = 20;       // Max matches that can be found
+const int MAXDICTWORDS = 30000;  // Max words that can be read in
 
-
-// A new function that helps to write into "dict", also making sure the number of lines
-// do not exceed MAXDICTWORDS.
-void countLines(istream& dictfile, string dict[], int& linecount)
-{
-	if (linecount >= MAXDICTWORDS) {
-		cout << "Word count exceeds max!" << endl;
-		exit(1);
-	}
-	if (!dictfile) return; // Stop when dictfile is read up.
-	dictfile >> dict[0];
-	linecount++;
-	countLines(dictfile, dict + 1, linecount);
-}
-
-// Puts each string in dictfile into the array dict. Returns the number of words
-// read into dict.This number should not be larger than MAXDICTWORDS since that is
-// the size of the array.
-int vocabularyCreator(istream& dictfile, string dict[]) {
-	int linecount = 0;
-	countLines(dictfile, dict, linecount);// Use the countLines function to write "dict".
-	return linecount - 1;
-}
-
-
-
+size_t countLines(istream &dictfile, string dict[], size_t maxSize);
+void loop(size_t i, const string& prefix, const string& rest, string result[],
+          const string dict[], size_t size, int &count);
+void printPermutations(string result[], const string dict[], size_t size, const string& prefix, const string& rest, int &count);
+void filter(vector<string>& output, const string dict[], size_t length, size_t dictCount);
+using COND = function<bool(const string &word)>;
+bool search(const COND &cond, const string list[], size_t size, size_t steps);
+bool match(const string &word, const string list[], size_t size,
+           size_t steps = 1);
+bool prefixMatch(const string &prefix, const string list[], size_t size,
+                 size_t steps = 1);
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// 
-//     For each character in rest{
-//       Add the character to the end of prefix.
-//       Remove character from rest.
-//       Use recursion to generate permutations with the updated values for prefix and rest.
-//     }
-void loop(size_t i, size_t max, string prefix, string rest) {
-	if (i >= max) {
-		return;
-	}
-	std::string newPrefix = prefix + rest.substr(i, 1);
-	std::string newRest = rest.substr(0, i) + rest.substr(i + 1);
-	i++;
-	loop(i, newRest.length(), newPrefix, newRest);
-
+//
+// Puts each string in dictfile into the array dict. Returns the number of words
+// read into dict.This number should not be larger than MAXDICTWORDS since that
+// is the size of the array.
+int vocabularyCreator(istream &dictfile, string dict[]) {
+  return countLines(dictfile, dict, MAXDICTWORDS);  // Use the countLines function to write "dict".
 }
 
-// print permutations of string
-// 
-// void printPermutations(string prefix, string rest) {
-// if (rest is empty) {
-//    Display the prefix string.
-// }
-// else {
-//     For each character in rest{
-//       Add the character to the end of prefix.
-//       Remove character from rest.
-//       Use recursion to generate permutations with the updated values for prefix and rest.
-//     }
-// }
-// }
-void printPermutations(vector<string>& result, const string& word, string prefix, string rest) {
-	if (rest.empty()) {
-		result.push_back(prefix);
-		return;
-	}
-	else {
-		loop(0, rest.length(), prefix, rest);
-	}
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Puts all the possibilities of word which are found in dict into results.
+// Returns the number of matched words found.This number should not be larger
+// than MAXRESULTS since that is the size of the array.The size is the number of
+// words inside the dict array.
+int potentialSequences(string word, const string dict[], int size,
+                       string results[]) {
+  int count = 0;
+  vector<string> filteredDict;
+
+  filter(filteredDict, dict, word.length(), size);
+
+  printPermutations(results, filteredDict.data(), filteredDict.size(), "", word,
+                    count);
+
+  return count;
 }
 
-// given "word", match it to the "dict" from the last word of "dict". If there is a match, increase the counter and write into results.
-void match(string word, const string dict[], int size, string results[])
-{
-	if (size == 0)
-	{
-		return; // when the size of dictionary becomes 0, it means no matches. So just return.
-	}
-	if (word == dict[size - 1]) // When there is a match
-	{
-		if (results->size() == MAXRESULTS) {
-			return;
-		}
-		results[results->size()] = word; // write into results
-		return;
-	}
-	else {
-		match(word, dict, size - 1, results);
-	}
-
-}
-
-void searchDict(size_t i, size_t max, vector<string> permutations, const string dict[], int size, string results[]) {
-	if (i >= max) {
-		return;
-	}
-	else {
-		match(permutations[i], dict, size, results);
-	}
-	searchDict(i + 1, max, permutations, dict, size, results);
-}
-
-
-// Puts all the possibilities of word which are found in dict into results. Returns
-// the number of matched words found.This number should not be larger than
-// MAXRESULTS since that is the size of the array.The size is the number of words
-// inside the dict array.
-int potentialSequences(string word, const string dict[], int
-	size, string results[]) {
-	vector<string> permutations;
-
-	printPermutations(permutations, word, "", word);
-
-	searchDict(0, permutations.size(), permutations, dict, size, results);
-
-	return results->size();
-}
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
 // Displays size number of strings from results. The results can be printed in
 // any order.
 void outcomeDisclosure(const string results[], int size) {
-	if (size < 0) {
-		return;
-	}
-	cout << results[--size] << endl;
-	outcomeDisclosure(results, size);
+  if (size == 0) {
+    return;
+  }
+  cout << "Matching word " << results[size - 1] << endl;
+  outcomeDisclosure(results, size - 1);
 }
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// helper functions:
+//
+//
+// 
 
-/*int main()
-{
-	string results[MAXRESULTS];
-	string dict[MAXDICTWORDS];
-	ifstream dictfile;         // file containing the list of words
-	int nwords;                // number of words read from dictionary
-	string word;
-
-	dictfile.open("words.txt");
-	if (!dictfile) {
-		cout << "File not found!" << endl;
-		return (1);
-	}
-
-	nwords = vocabularyCreator(dictfile, dict);
-
-	cout << "Please enter a string for an anagram: ";
-	cin >> word;
-
-	int numMatches = potentialSequences(word, dict, nwords, results);
-	if (!numMatches) {
-		cout << "No matches found" << endl;
-	}
-	else {
-		outcomeDisclosure(results, numMatches);
-	}
-
-	return 0;
-}
+/**
+ * @brief puts dictfile lines into dict
+ * @param dictfile file read from text
+ * @param dict words to search from
+ * @param maxSize maximum number of allowed words in dict
+ * @return number of lines in dict
 */
+size_t countLines(istream &dictfile, string dict[], size_t maxSize) {
+  if (dictfile.eof() || maxSize == 0) {
+    return 0;
+  }
+
+  if (getline(dictfile, dict[0])) {
+    return countLines(dictfile, &dict[1], maxSize - 1) + 1;
+  }
+
+  return 0;
+}
+
+/**
+ * @brief create permutations by switching letters
+ * @param i index to make sure iterations don't go over length of rest
+ * @param beginning of word
+ * @param rest remainder of word 
+ * @param result list of permutations that exist in dict
+ * @param dict words to search from
+ * @param size number of words in dict
+ * @param count how many words in result
+*/
+void loop(size_t i, const string& prefix, const string& rest, string result[],
+          const string dict[], size_t size, int &count) {
+  if (i >= rest.length()) {
+    return;
+  }
+  std::string newPrefix = prefix + rest.substr(i, 1);
+  std::string newRest = rest.substr(0, i) + rest.substr(i + 1);
+  i++;
+  printPermutations(result, dict, size, newPrefix, newRest, count);
+  loop(i, prefix, rest, result, dict, size, count);
+}
+
+/**
+ * @brief find word in dictionary
+ * @param word the word to be searched for
+ * @param dict words to search from
+ * @param size number of words in dict
+ * @return true if word and word in dict are the same
+*/
+bool match(const string &word, const string dict[], int size) {
+  if (size == 0) {
+    return false;
+  }
+  if (word == dict[size - 1]) {
+    return true;
+  }
+  return match(word, dict, size - 1);
+}
+
+/**
+ * @brief puts permutations into results if it matches a word
+ * @param result list of permutations that exist in dict
+ * @param dict words to search from
+ * @param size number of words in dict
+ * @param prefix beginning of word
+ * @param rest remainder of word
+ * @param count how many words in result
+*/
+void printPermutations(string result[], const string dict[], size_t size, const string& prefix, const string& rest, int &count) {
+  if (count >= MAXRESULTS) {
+    return;
+  }
+  if (rest.empty()) {
+    if (!match(prefix, result, count) && match(prefix, dict, size)) {
+      result[count++] = prefix;
+    }
+    return;
+  }
+  if (prefixMatch(prefix, dict, size, 100)) {
+    // continue tring more permutations only if the prefix matches
+    loop(0, prefix, rest, result, dict, size, count);
+  }
+}
+
+/**
+ * @brief checks dict for words of the same size
+ * @param output the list of words in dict that are the same size as the length
+ * @param dict the dictionary or words
+ * @param length the length of the word
+ * @param dictCount the number of words in dict
+*/
+void filter(vector<string>& output, const string dict[], size_t length, size_t dictCount) {
+  auto lengthMatch = [&output, &length](const string &value) {
+    if (length == value.length()) {
+      output.push_back(value);
+    }
+    return false;
+  };
+  search(lengthMatch, dict, dictCount, 100);
+}
+
+/**
+ * @brief find a word in an array
+ * @param word the word to search
+ * @param list the list of words
+ * @param size the number of words in the array
+ * @param steps number of words to process in a batch
+ * @return true if found
+ */
+bool match(const string &word, const string list[], size_t size, size_t steps) {
+  auto wholeWordMatch = [&word](const string &value) { return word == value; };
+  return search(wholeWordMatch, list, size, steps);
+}
+
+/**
+ * @brief check there is any word in the array has the prefix
+ * @param prefix the prefix
+ * @param list the list of words
+ * @param size the number of words in the array
+ * @param steps number of words to process in a batch
+ * @return true if found
+ */
+bool prefixMatch(const string &prefix, const string list[], size_t size,
+                 size_t steps) {
+  if (prefix.empty()) {
+    return true;
+  }
+
+  auto prefixMatch = [&prefix](const string &value) {
+    return prefix == value.substr(0, prefix.length());
+  };
+  return search(prefixMatch, list, size, steps);
+}
+
+/**
+ * @brief search the array by condition
+ * @param cond the critieria to satisfy
+ * @param list the list of strings
+ * @param size the number of elements in the list
+ * @param steps number of words to process in a batch
+ * @return true if found
+*/
+bool search(const COND &cond, const string list[], size_t size, size_t steps) {
+  if (0 == size) {
+    return false;
+  }
+
+  if (size > steps && steps > 64) {
+    if (search(cond, list, steps, 1)) {
+      return true;
+    }
+    return search(cond, &list[steps], size - steps, steps);
+  }
+
+  if (cond(list[size - 1])) {
+    return true;
+  }
+
+  return search(cond, list, size - 1, steps);
+}
--- Project 3/main.cpp
+++ Project 3/main.cpp
@@ -1,15 +1,17 @@
 #include "anagrams.h"
+#include "permutation.h"
 #include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
+#include "main.h"
 
 using namespace std;
 
+
 int main() {
-	
     string results[MAXRESULTS];
-    string dict[MAXDICTWORDS];
+    vector<string> dict(MAXDICTWORDS);
     ifstream dictfile;         // file containing the list of words
     int nwords;                // number of words read from dictionary
     string word;
@@ -20,19 +22,17 @@ int main() {
         return (1);
     }
 
-    nwords = vocabularyCreator(dictfile, dict);
+    nwords = vocabularyCreator(dictfile, dict.data());
 
     cout << "Please enter a string for an anagram: ";
     cin >> word;
-
-    int numMatches = potentialSequences(word, dict, nwords, results);
+    
+    int numMatches = potentialSequences(word, dict.data(), nwords, results);
     if (!numMatches) {
         cout << "No matches found" << endl;
     }
     else {
         outcomeDisclosure(results, numMatches);
     }
-
     return 0;
-
 }
